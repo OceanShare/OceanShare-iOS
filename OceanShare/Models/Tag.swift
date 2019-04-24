6f1ecc1e78218ecdb6faeda47c4b9d4bef7d43f0
Hugo Lackermaier <hugo.lackermaier@epitech.eu>
2019-04-24 13:35:03 +0300

    Add new icons + map fonctionnality

---
+++ OceanShare/Models/Tag.swift
@@ -0,0 +1,18 @@
+//
+//  Tag.swift
+//  OceanShare
+//
+//  Created by Hugo Lackermaier on 19/03/2019.
+//  Copyright © 2019 Joseph Pereniguez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Mapbox
+
+struct Tag {
+    var id: Int?
+    var description: String?
+    var x: Double?
+    var y: Double?
+}
