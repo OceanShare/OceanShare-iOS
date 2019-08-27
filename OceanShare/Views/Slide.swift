3946f68f561754181b7755127e4694545141ceea
Estayparadox <pereniguez.joseph@hotmail.fr>
2019-08-27 14:30:33 +0200

    [+] user walkthrough

---
+++ OceanShare/Views/Slide.swift
@@ -0,0 +1,17 @@
+//
+//  Slide.swift
+//  OceanShare
+//
+//  Created by Joseph Pereniguez on 26/08/2019.
+//  Copyright © 2019 Joseph Pereniguez. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
