f9b81702cd33ec5132485feb99b5802b1a47b5eb
Estayparadox <pereniguez.joseph@hotmail.fr>
2019-09-26 08:07:05 +0200

    [~] update api

--- OceanShare/Models/Registry.swift
+++ OceanShare/Models/Registry.swift
@@ -56,7 +56,7 @@ struct Registry {
     let websiteUrl = "https://sagotg.github.io/OceanShare/"
     
     // oceanshare api url
-    let apiUrl = "http://104.197.254.106:5000/api/weather"
+    let apiUrl = "http://35.226.11.123:5000/api/weather"
     
     // default event descriptions
     let descJellyfishs = NSLocalizedString("descJellyfishs", comment: "")
