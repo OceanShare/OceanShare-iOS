72f1dee57dfbb8b8bb1249d87577d57933939d57
Estayparadox <pereniguez.joseph@hotmail.fr>
2019-12-05 13:19:52 +0100

    [±] init devel

---
+++ OceanShare/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  OceanShare
+//
+//  Created by Joseph Pereniguez on 18/06/2019.
+//  Copyright © 2019 Joseph Pereniguez. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ OceanShare/Models/Offers.swift
@@ -0,0 +1,9 @@
+//
+//  Offers.swift
+//  OceanShare
+//
+//  Created by Joseph Pereniguez on 29/11/2019.
+//  Copyright © 2019 Joseph Pereniguez. All rights reserved.
+//
+
+import Foundation
