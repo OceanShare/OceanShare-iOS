438ca1024d6bf3f614f8fd231169645aeb7a2b1c
Estayparadox <pereniguez.joseph@hotmail.fr>
2019-12-09 20:38:42 +0100

    [+] Documentation

--- OceanShare/ViewControllers/PrivateMessageViewController.swift
+++ OceanShare/ViewControllers/PrivateMessageViewController.swift
@@ -13,11 +13,13 @@ class PrivateMessageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         overrideUserInterfaceStyle = .light
         
     }
     
+    /**
+     - Description - Hide the view.
+     */
     @IBAction func closeView(_ sender: Any) {
         let mainTabBarController = storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") as! MainTabBarController
         mainTabBarController.selectedViewController = mainTabBarController.viewControllers?[0]
